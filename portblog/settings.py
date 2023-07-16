6d8daa58aa05c84ce6279ba021d38ce07f6a4ea3
Spencer Schmidt <spennccee.schmidt0@gmail.com>
2023-07-16 19:12:36 -0400

    d

--- portblog/settings.py
+++ portblog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 WSGI_APPLICATION = 'vercel_app.wsgi.app'
@@ -22,16 +21,10 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600)
-
-}
 
 # Application definition
 
