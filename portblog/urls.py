a36db6258bec8f3b26630264dd078ecfdbe965f9
Spencer Schmidt <spennccee.schmidt0@gmail.com>
2023-07-18 14:41:19 -0400

    update

--- portblog/urls.py
+++ portblog/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for portblog project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from blog.views import PostListAPIView, PostDetailAPIView, PostByTagAPIView
 from django.contrib import admin
@@ -22,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', PostListAPIView.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetailAPIView.as_view(), name='post-detail'),
-    path('posts/tag/<int:tag_id>/', PostByTagAPIView.as_view(), name='posts_by_tag_api'),
+    path('posts/tag/<int:tag_id>/', PostByTagAPIView.as_view(), name='posts-by-tag-api'),
 ]
