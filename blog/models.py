75c98a3dc818582d18abfa76bef14fb4a6f72d31
Spencer Schmidt <spennccee.schmidt0@gmail.com>
2023-07-19 16:11:37 -0400

    added subtitle field

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Tag(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     tags = models.ManyToManyField(Tag)
